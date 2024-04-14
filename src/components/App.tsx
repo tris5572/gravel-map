c4fdcee5d354b9d7e79015b3f37910c7b9a77ef5
tris <nexus4fortune@gmail.com>
2024-04-14 12:22:08 +0900

    feat: 線を描画するテスト

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,35 +3,39 @@ import Map from 'react-map-gl/maplibre';
 
 import 'maplibre-gl/dist/maplibre-gl.css';
 import './App.css';
+import { pathLayers } from '../data/data';
 // import { RoutesView } from './RoutesView';
 
 export function App() {
-  return (
-    // <div style={{ width: '100dvw', height: '100dvh', position: 'absolute', left: 0, top: 0 }}>
+  const paths = pathLayers();
+  console.log(paths);
 
-    <DeckGL
-      initialViewState={{
-        longitude: 139.75,
-        latitude: 35.685,
-        zoom: 12,
-      }}
-      controller={true}
-    >
-      <Map
-        reuseMaps
-        // initialViewState={{
-        //   longitude: 139.75,
-        //   latitude: 35.685,
-        //   zoom: 12,
-        // }}
-        mapStyle="https://tile.openstreetmap.jp/styles/maptiler-basic-ja/style.json"
-        // onClick={(e) => {
-        //   console.log(e.features);
-        // }}
+  return (
+    <div style={{ width: '100dvw', height: '100dvh', position: 'absolute', left: 0, top: 0 }}>
+      <DeckGL
+        initialViewState={{
+          longitude: 139.75,
+          latitude: 35.685,
+          zoom: 12,
+        }}
+        controller={true}
+        layers={paths}
       >
-        {/* <RoutesView /> */}
-      </Map>
-    </DeckGL>
-    // </div>
+        <Map
+          reuseMaps
+          // initialViewState={{
+          //   longitude: 139.75,
+          //   latitude: 35.685,
+          //   zoom: 12,
+          // }}
+          mapStyle="https://tile.openstreetmap.jp/styles/maptiler-basic-ja/style.json"
+          // onClick={(e) => {
+          //   console.log(e.features);
+          // }}
+        >
+          {/* <RoutesView /> */}
+        </Map>
+      </DeckGL>
+    </div>
   );
 }
---
+++ src/data/data.ts
@@ -0,0 +1,40 @@
+import { PathLayer } from '@deck.gl/layers';
+
+type PathData = {
+  coordinates: [number, number][];
+};
+
+const DATA: PathData[] = [
+  {
+    coordinates: [
+      [139.7345854, 35.7056804],
+      [139.7658193, 35.6708245],
+    ],
+  },
+  {
+    coordinates: [
+      [139.7488565, 35.6709639],
+      [139.7958789, 35.7139042],
+    ],
+  },
+];
+
+export function pathLayers(): PathLayer[] {
+  const layers = [];
+
+  const layer = new PathLayer<PathData>({
+    id: 'path',
+    data: DATA,
+    getColor: [255, 0, 0, 150],
+    getPath: (d: PathData) => {
+      return d.coordinates;
+    },
+    getWidth: 6,
+    pickable: true,
+    widthUnits: 'pixels',
+    capRounded: true,
+  });
+  layers.push(layer);
+
+  return layers;
+}
