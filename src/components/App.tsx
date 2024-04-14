93ccd4f462478133dab16c3bd0584b64fe98f93b
tris <nexus4fortune@gmail.com>
2024-04-15 07:51:08 +0900

    feat: データを元に経路を仮表示

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,10 @@
 import DeckGL from '@deck.gl/react';
 import Map from 'react-map-gl/maplibre';
-
 import 'maplibre-gl/dist/maplibre-gl.css';
 import './App.css';
-import { getTooltip, pathLayers } from '../data/data';
-// import { RoutesView } from './RoutesView';
+import { getTooltip, layerFromRouteData } from '../data/data';
 
 export function App() {
-  const paths = pathLayers();
-  console.log(paths);
-
   return (
     <div style={{ width: '100dvw', height: '100dvh', position: 'absolute', left: 0, top: 0 }}>
       <DeckGL
@@ -19,23 +14,10 @@ export function App() {
           zoom: 12,
         }}
         controller={true}
-        layers={paths}
+        layers={[layerFromRouteData()]}
         getTooltip={getTooltip}
       >
-        <Map
-          reuseMaps
-          // initialViewState={{
-          //   longitude: 139.75,
-          //   latitude: 35.685,
-          //   zoom: 12,
-          // }}
-          mapStyle="https://tile.openstreetmap.jp/styles/maptiler-basic-ja/style.json"
-          // onClick={(e) => {
-          //   console.log(e.features);
-          // }}
-        >
-          {/* <RoutesView /> */}
-        </Map>
+        <Map reuseMaps mapStyle="https://tile.openstreetmap.jp/styles/maptiler-basic-ja/style.json" />
       </DeckGL>
     </div>
   );
--- src/data/data.ts
+++ src/data/data.ts
@@ -1,52 +1,111 @@
 import { PathLayer } from '@deck.gl/layers';
-
 import type { PickingInfo } from '@deck.gl/core';
 
-type PathData = {
-  coordinates: [number, number][];
-};
+import FILE from './data.json';
 
-const DATA: PathData[] = [
-  {
-    coordinates: [
-      [139.7345854, 35.7056804],
-      [139.7658193, 35.6708245],
-    ],
-  },
-  {
-    coordinates: [
-      [139.7488565, 35.6709639],
-      [139.7958789, 35.7139042],
-    ],
-  },
-];
+// -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
 
-export function pathLayers(): PathLayer[] {
-  const layers = [];
-
-  const layer = new PathLayer<PathData>({
-    id: 'path',
-    data: DATA,
-    getColor: [255, 0, 0, 150],
-    getPath: (d: PathData) => {
-      return d.coordinates;
-    },
-    getWidth: 6,
-    pickable: true,
-    widthUnits: 'pixels',
-    capRounded: true,
-  });
-  layers.push(layer);
-
-  return layers;
-}
-
+/**
+ * ツールチップを生成する。
+ * TODO: 現時点では仮実装。きちんと実装する。
+ */
 export function getTooltip({ object }: PickingInfo) {
   if (!object) {
     return null;
   }
-  // else if(object instanceof PathData){
-
-  // }
   return JSON.stringify(object);
+}
+
+// -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
+
+/**
+ * ファイルのデータを元に、レイヤーを生成する。
+ */
+export function layerFromRouteData(): PathLayer {
+  return new PathLayer<RouteData>({
+    id: 'route-path',
+    data: routeDataFromFile(),
+    getColor: [255, 0, 0, 150],
+    getPath: (d: RouteData) => {
+      return d.coordinates;
+    },
+    getWidth: 10,
+    pickable: true,
+    widthUnits: 'pixels',
+    capRounded: true,
+    jointRounded: true,
+  });
+}
+
+// -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
+
+/**
+ * データファイルのルートの型
+ */
+type FileRoute = {
+  name: string;
+  gra: number;
+  wid: number;
+  slp: number;
+  desc: string;
+  cods: [number, number][] | [];
+};
+
+/**
+ * ルートデータの型
+ */
+type RouteData = {
+  /** 名前 */
+  name: string;
+  /** グラベルレベル */
+  gravel: number;
+  /** 道幅レベル */
+  width: number;
+  /** 斜度レベル */
+  slope: number;
+  /** 説明 */
+  description: string;
+  /** 座標の並び */
+  coordinates: [number, number][];
+};
+
+/**
+ * ファイルのルートデータをパースし、アプリで使用するルートデータを生成する
+ */
+function parseRouteData(source: FileRoute): RouteData | undefined {
+  if (source.cods.length === 0) {
+    return undefined;
+  }
+
+  // 緯度と経度を逆転させる（データは「緯度・経度」の順だが、MapLibreでは「経度・緯度」であるため）
+  const coordinates: [number, number][] = [];
+  for (const c of source.cods) {
+    coordinates.push([c[1], c[0]]);
+  }
+
+  return {
+    name: source.name,
+    gravel: source.gra,
+    width: source.wid,
+    slope: source.slp,
+    description: source.desc,
+    coordinates,
+  };
+}
+
+/**
+ * ファイルの情報を元に、ルートデータを生成する
+ */
+function routeDataFromFile(): RouteData[] {
+  const list = [];
+
+  for (const r of FILE.routes) {
+    const data = r as FileRoute;
+    const d = parseRouteData(data);
+    if (d !== undefined) {
+      list.push(d);
+    }
+  }
+
+  return list;
 }
