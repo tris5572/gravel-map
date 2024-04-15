bf8f944af732a9d4a22efefe62fc5fb0740ebb20
tris <nexus4fortune@gmail.com>
2024-04-16 06:16:18 +0900

    feat: 初期表示位置を調整

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,9 +9,9 @@ export function App() {
     <div style={{ width: '100dvw', height: '100dvh', position: 'absolute', left: 0, top: 0 }}>
       <DeckGL
         initialViewState={{
-          longitude: 139.75,
-          latitude: 35.685,
-          zoom: 12,
+          longitude: 139.54,
+          latitude: 35.74,
+          zoom: 9,
         }}
         controller={true}
         layers={[layerFromRouteData()]}
