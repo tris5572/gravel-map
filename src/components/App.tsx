6858e25f267b74b878c12f462b2a4cb5ab8190e6
tris <nexus4fortune@gmail.com>
2024-04-17 06:57:43 +0900

    feat: MapLibre のコントロールを有効化

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,24 +1,39 @@
-import DeckGL from '@deck.gl/react';
-import Map from 'react-map-gl/maplibre';
+import Map, { GeolocateControl, NavigationControl, ScaleControl, useControl } from 'react-map-gl/maplibre';
 import 'maplibre-gl/dist/maplibre-gl.css';
-import './App.css';
+import { DeckProps } from '@deck.gl/core';
+import { MapboxOverlay } from '@deck.gl/mapbox';
 import { getTooltip, layerFromRouteData } from '../data/data';
+import './App.css';
+
+/**
+ * MapLibre のコントロールを有効化するためのオーバーレイ
+ *
+ * @see https://deck.gl/docs/developer-guide/base-maps/using-with-maplibre
+ * @see https://deck.gl/docs/api-reference/mapbox/mapbox-overlay
+ */
+function DeckGLOverlay(props: DeckProps) {
+  const overlay = useControl<MapboxOverlay>(() => new MapboxOverlay(props));
+  overlay.setProps(props);
+  return null;
+}
 
 export function App() {
   return (
     <div style={{ width: '100dvw', height: '100dvh', position: 'absolute', left: 0, top: 0 }}>
-      <DeckGL
+      <Map
+        reuseMaps
         initialViewState={{
           longitude: 139.54,
           latitude: 35.74,
           zoom: 9,
         }}
-        controller={true}
-        layers={[layerFromRouteData()]}
-        getTooltip={getTooltip}
+        mapStyle="https://tile.openstreetmap.jp/styles/maptiler-basic-ja/style.json"
       >
-        <Map reuseMaps mapStyle="https://tile.openstreetmap.jp/styles/maptiler-basic-ja/style.json" />
-      </DeckGL>
+        <NavigationControl />
+        <GeolocateControl />
+        <ScaleControl />
+        <DeckGLOverlay layers={[layerFromRouteData()]} getTooltip={getTooltip} />
+      </Map>
     </div>
   );
 }
