50db26297e4b4c5081c4101b72e41c159ced04f3
tris <nexus4fortune@gmail.com>
2024-04-22 07:25:16 +0900

    chore: リポジトリ名の変更に伴い、デプロイ設定を変更

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   // GitHub Pages へデプロイする際のフォルダ名として、リポジトリ名を設定
-  const base = mode === 'production' ? '/gravel-map-react/' : undefined;
+  const base = mode === 'production' ? '/gravel-map/' : undefined;
 
   return {
     base,
